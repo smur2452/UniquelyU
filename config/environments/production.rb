5b18e655c96e3741ae48de92ee7513a80314b2c8
smur2452 <smur2452@gmail.com>
2016-03-10 01:17:26 +0000

    Nitrous.io setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://uniquelyu.herokuapp.com' }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :domain               => 'heroku.com',
+    :user_name            => ENV['USERNAME'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
